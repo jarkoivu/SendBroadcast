96519433df8d50f4245fdbb06f407790b5fd895d
jarkoivu <jarikoivula@hotmail.com>
2017-07-25 18:35:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jari/sendbroadcast/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.jari.sendbroadcast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendOutBroadcast(View view){
+        Intent i = new Intent();
+        i.setAction("com.example.jari.sendbroadcast");
+        i.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(i);
+    }
+}
